9884b0c857743e8ec6c3da7fdff4216564f67b21
Vernon Keenan <vern@taxnexus.net>
2019-06-24 21:22:23 +0000

    Move methods

--- force/stream.go
+++ force/stream.go
@@ -1,15 +1,9 @@
 package force
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"net/http/cookiejar"
-
-	"golang.org/x/net/publicsuffix"
-
-	//"reflect"
 	"strings"
 )
 
@@ -42,73 +36,6 @@ func (s *forceStreaming) connect() ([]byte, error) {
 	defer resp.Body.Close()
 
 	return respBytes, err
-}
-
-// ConnectToStreamingAPI connects a ForceAPI client
-func (forceAPI *API) ConnectToStreamingAPI() {
-	//set up the client
-	cookiejarOptions := cookiejar.Options{
-		PublicSuffixList: publicsuffix.List,
-	}
-	jar, _ := cookiejar.New(&cookiejarOptions)
-	forceAPI.stream = &forceStreaming{"", map[string]func([]byte, ...interface{}){}, 0, forceAPI, &http.Client{Jar: jar}}
-
-	//handshake
-	var params = `{"channel":"/meta/handshake", "supportedConnectionTypes":["long-polling"], "version":"1.0"}`
-	handshakeResp, _ := forceAPI.stream.httpPost(params)
-	handshakeBytes, _ := ioutil.ReadAll(handshakeResp.Body)
-	defer handshakeResp.Body.Close()
-
-	var data []map[string]interface{}
-	json.Unmarshal(handshakeBytes, &data)
-	fmt.Println(data)
-	forceAPI.stream.ClientID = data[0]["clientID"].(string)
-
-	//must handle error here
-
-	// connect
-	connBytes, _ := forceAPI.stream.connect()
-
-	var connectData []map[string]interface{}
-	json.Unmarshal(connBytes, &connectData)
-	for _, msg := range data {
-		cb := forceAPI.stream.Subscriptions[msg["channel"].(string)]
-		if cb != nil {
-			cb(connBytes)
-		}
-		fmt.Println(string(connBytes))
-	}
-
-	go func() {
-		// got to allow disconnect, handle errors
-		for {
-			connBytes, _ = forceAPI.stream.connect()
-			json.Unmarshal(connBytes, &connectData)
-
-			for _, msg := range connectData {
-				cb := forceAPI.stream.Subscriptions[msg["channel"].(string)]
-				if cb != nil {
-					cb(connBytes)
-				}
-			}
-			//fmt.Println(string(connBytes))
-		}
-	}()
-}
-
-// SubscribeToPushTopic here we have to allow the ability to pass in a callback function
-func (forceAPI *API) SubscribeToPushTopic(pushTopic string, callback func([]byte, ...interface{})) ([]byte, error) {
-	topicString := "/topic/" + pushTopic
-	subscribeParams := `{ "channel": "/meta/subscribe", "clientID": "` + forceAPI.stream.ClientID + `", "subscription": "` + topicString + `"}`
-
-	subscribeResp, _ := forceAPI.stream.httpPost(subscribeParams)
-	subscribeBytes, err := ioutil.ReadAll(subscribeResp.Body)
-
-	defer subscribeResp.Body.Close()
-
-	forceAPI.stream.Subscriptions[topicString] = callback
-	return subscribeBytes, err
-
 }
 
 // UnsubscribeFromPushTopic is a func that doesn't do anything yet
